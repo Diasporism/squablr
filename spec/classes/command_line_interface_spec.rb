42c757e97bc599f6dd475bcc79a1956e87139abb
Logan Sears <lsears@itriagehealth.com>
2014-10-02 13:49:19 -0600

    First failing spec. TODO: Implement this.

--- spec/classes/command_line_interface_spec.rb
+++ spec/classes/command_line_interface_spec.rb
@@ -2,4 +2,10 @@ require_relative '../spec_helper'
 
 describe CommandLineInterface do
 
+  describe 'initialize' do
+    it 'holds the current input from the user' do
+      expect(described_class.new.last_input).to eq ''
+    end
+  end
+
 end
