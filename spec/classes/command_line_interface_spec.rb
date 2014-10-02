add0b7a79e0bccf5736e3c1f305d1ff228eb45b0
Aimee Maher <amaher@itriagehealth.com>
2014-10-02 13:57:39 -0600

    adds another test

--- spec/classes/command_line_interface_spec.rb
+++ spec/classes/command_line_interface_spec.rb
@@ -1,11 +1,17 @@
 require_relative '../spec_helper'
 
 describe CommandLineInterface do
+  context 'starting the app' do
+    describe 'last input' do
+      it 'holds initial input from the user' do
+        expect(described_class.new.last_input).to eq ''
+      end
 
-  describe 'initialize' do
-    it 'holds the current input from the user' do
-      expect(described_class.new.last_input).to eq ''
+      it 'updates last input when new input is given' do
+        cli = described_class.new
+        cli.last_input = "Puppies"
+        expect(cli.last_input).to eq 'Puppies'
+      end
     end
   end
-
 end
