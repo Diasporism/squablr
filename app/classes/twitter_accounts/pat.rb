5cc6798d554dfa507df3432df1e355e302cd8420
Logan Sears <lsears@itriagehealth.com>
2014-10-07 12:45:50 -0600

    Remove Pat and Sam inheritance of TwitterAccount since it is no longer needed. Pat and Sam now have their own api keys and credentials to tweet at each other with. These are located in the .env file.

--- app/classes/twitter_accounts/pat.rb
+++ app/classes/twitter_accounts/pat.rb
@@ -1,4 +1,13 @@
-class Pat < TwitterAccount
+class Pat
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['PAT_CONSUMER_KEY']
+      config.consumer_secret     = ENV['PAT_CONSUMER_SECRET']
+      config.access_token        = ENV['PAT_ACCESS_TOKEN']
+      config.access_token_secret = ENV['PAT_ACCESS_TOKEN_SECRET']
+    end
+  end
+
   def tweet_sweet_nothing
     client.update('@RoboSam3000 If nothing lasts forever, can I be your nothing?')
   end
--- app/classes/twitter_accounts/sam.rb
+++ app/classes/twitter_accounts/sam.rb
@@ -1,4 +1,13 @@
-class Sam < TwitterAccount
+class Sam
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['SAM_CONSUMER_KEY']
+      config.consumer_secret     = ENV['SAM_CONSUMER_SECRET']
+      config.access_token        = ENV['SAM_ACCESS_TOKEN']
+      config.access_token_secret = ENV['SAM_ACCESS_TOKEN_SECRET']
+    end
+  end
+
   def tweet_sweet_nothing
     client.update('@RoboPat3000 If nothing lasts forever, can I be your nothing?')
   end
